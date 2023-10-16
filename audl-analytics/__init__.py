f9d63007fd687c2e2b306d506cc5bdfd5b32310a
Emulie Chhor <emulie.chhor@hotmail.com>
2023-10-15 20:06:29 -0400

    calling definitions from etl

--- audl-analytics/__init__.py
+++ audl-analytics/__init__.py
@@ -1,12 +1,12 @@
-from dagster import Definitions, ScheduleDefinition, define_asset_job, load_assets_from_package_module
+from dagster import Definitions, ScheduleDefinition, define_asset_job, load_assets_from_modules, AssetSelection
 
-from . import assets
-#  from assets import etl
+#  from . import assets
+from .assets import etl
 
+#  --- ETL Job: “At 18:00 on Monday.” 
+etl_assets = load_assets_from_modules([etl])
+etl_job = define_asset_job("etl_job", selection=AssetSelection.all())
+etl_schedule = ScheduleDefinition(job=etl_job, cron_schedule='0 18 * * 1')
 
-# “At 18:00 on Monday.” 
-etl_schedule = ScheduleDefinition(job=define_asset_job(name='etl_job'),
-                                      cron_schedule='0 18 * * 1')
+defs = Definitions(assets=etl_assets, schedules=[etl_schedule])
 
-defs = Definitions(assets=load_assets_from_package_module(assets.etl), 
-                   schedules=[etl_schedule])
