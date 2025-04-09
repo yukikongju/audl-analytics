a1907d5cae548486c9529abd63c10302697602bd
yukikongju <emulie.chhor@hotmail.com>
2025-04-09 12:49:32 -0400

    added draft for create games

---
+++ src/db/mongodb/create_games.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+from src.utils.connections import get_mongo_db
+
+def main():
+    db = get_mongo_db()
+    ROOT_DIR = os.path.abspath(os.curdir)
+    GAME_SCHEMA_JSON_PATH = os.path.join(ROOT_DIR, "src/db/mongodb/schema/games.json")
+
+    try: 
+        with open(GAME_SCHEMA_JSON_PATH) as f:
+            game_schema = json.load(f)
+    except Exception as e:
+        raise FileNotFoundError(f"Could not find file: {e}")
+
+    db.create_collection("games", game_schema)
+
+if __name__ == "__main__":
+    main()
