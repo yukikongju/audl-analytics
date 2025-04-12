a4f6b3941512298f90f26bd57de6c3bdc5dca519
yukikongju <emulie.chhor@hotmail.com>
2025-04-12 11:11:49 -0400

    added players schema

---
+++ src/db/postgres/schema/players.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS players (
+    ext_player_id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    team TEXT NOT NULL,
+    jersey_number TEXT,
+    height TEXT,
+    weight FLOAT,
+    dob TEXT,
+    college TEXT,
+    hometown TEXT,
+    bio TEXT,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+)
