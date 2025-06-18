52193528afeb55b24382cd292ed3249d04c315ac
yukikongju <emulie.chhor@hotmail.com>
2025-06-18 13:38:25 -0400

    added function to get all player ext ids

---
+++ src/extract/audl_utils.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+from typing import List
+
+def get_all_player_ext_ids() -> List[str]:
+    page_idx = 0
+    page_empty = False
+    ext_player_ids = []
+    while not page_empty:
+        url = f"https://www.watchufa.com/league/players?page={page_idx}"
+        print(url)
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        a_tags = soup.find_all('a', href=True)
+        players = [tag['href'].split('/')[-1] for tag in a_tags if tag['href'].startswith('/league/players/')]
+        if len(players) == 0:
+           page_empty = True 
+        ext_player_ids += players
+        page_idx += 1
+    return ext_player_ids
